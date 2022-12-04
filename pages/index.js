1fa43593cc6588f312c5f4b01098826e01a2ddd3
JHollis <85080306+hollisj92@users.noreply.github.com>
2022-12-04 14:39:44 -0500

    PG 13 rating

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>We are learning shit quickly.</p>
+        <p>We are learning @#$%&! quickly.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
